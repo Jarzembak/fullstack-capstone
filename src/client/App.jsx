6c4414c2899373d4a940e457c478fb1a00c1bbfa
James Boyd <jamesboyd2011@live.com>
2024-04-18 21:32:56 -0400

    Created Register component

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,7 +11,8 @@ function App() {
         <h1>Boilerplate</h1>
         <img id='comp-img' src='./computer.png'></img>
         <p>Replace the starter code in this template with something cool</p>
-        <Login />
+        {/* <Login /> */}
+        <Register />
     </div>
   );
 }
