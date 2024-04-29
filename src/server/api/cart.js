490cb3de7f79427c6628179468f5d2c16f312383
dsmartin1 <dsmtheman@yahoo.com>
2024-04-29 18:08:12 -0400

    Updated prisma implementation, ./src/server restructured, basic GET routes added

---
+++ src/server/api/cart.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const router = express.Router();
+const {PrismaClient} = require("@prisma/client");
+const prisma = new PrismaClient();
+
+// GET all carts
+router.get('/', async (req, res, next) => {
+    try {
+        const result = await prisma.cart.findMany();
+        res.send(result);
+    } catch (error) {
+        next(error)
+    }
+})
+
+// GET cart by cartId in request
+router.get('/:cartId', async (req, res, next) => {
+    try {
+        const result = await prisma.cart.findUnique({
+            where: {
+                cartId: Number(req.params.cartId),
+            },
+        });
+        res.send(result);
+    }
+    catch (error) {
+        next(error)
+    }
+});
+
+// GET carts by userId in request
+router.get('/:userId', async (req, res, next) => {
+    try {
+        const result = await prisma.cart.findMany({
+            where: {
+                userId: Number(req.params.userId),
+            },
+        });
+        res.send(result);
+    }
+    catch (error) {
+        next(error)
+    }
+});
+
+// GET 'current' cart by userId in request
+// There should be ONLY ONE with cartStatus 'current'
+router.get('/:userId/current', async (req, res, next) => {
+    try {
+        const result = await prisma.cart.findUnique({
+            where: {
+                userId: Number(req.params.userId),
+                cartStatus: 'current',
+            },
+        });
+        res.send(result);
+    }
+    catch (error) {
+        next(error)
+    }
+});
+
+// TODO - routes requiring authentication
+
+module.exports = router;
---
+++ src/server/api/cartItem.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const {PrismaClient} = require("@prisma/client");
+const prisma = new PrismaClient();
+
+// GET all cartItems
+router.get('/', async (req, res, next) => {
+    try {
+        const result = await prisma.cartItem.findMany();
+        res.send(result);
+    } catch (error) {
+        next(error)
+    }
+})
+
+// GET cartItems by cartId
+router.get('/:cartId', async (req, res, next) => {
+    try {
+        const result = await prisma.cartItem.findMany({
+            where: {
+                cartId: Number(req.params.cartId),
+            },
+        });
+        res.send(result);
+    }
+    catch (error) {
+        next(error)
+    }
+});
+
+// GET cartItems by userId
+router.get('/:userId', async (req, res, next) => {
+    try {
+        const result = await prisma.cartItem.findMany({
+            where: {
+                userId: Number(req.params.userId),
+            },
+        });
+        res.send(result);
+    }
+    catch (error) {
+        next(error)
+    }
+});
+
+// TODO - routes requiring authentication
+
+module.exports = router;
